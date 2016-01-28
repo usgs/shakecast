fb935f13dfd8159bd81c1d8cd2eff3d454edbf24
Daniel A Slosky <dslosky@usgs.gov>
2016-01-28 10:57:46 -0700

    Re-worked docstrings for app.__init__ and New_Thread to work better with Sphinx

--- sc/app/__init__.py
+++ sc/app/__init__.py
@@ -2,20 +2,26 @@
 This app package contains all the code written for pyCast
 (ShakeCast V4).
 
-How to start the application after pulling from source:
-    1. start the server
-        Navigate to shakecast_v4/sc/app/
+Start the application after pulling from source:
+    1. Start the server
+    ::
+        $ cd /some/dir/shakecast_v4/sc/app/
         $ python server.py
-    2. start the CLI
-        Navigate to shakecast_v4/sc/
+        
+    2. Start the CLI
+    ::
+        $ cd /some/dir/shakecast_v4/sc/        
         $ python ui.py
-    3. start USGS monitor (geo_json) and shakemap monitor (check_new_shakemaps)
-        From within the CLI:
-            ShakeCast> start
-    4. upload user data:
-        (API under construction and *not ready for use*)
-        From within the CLI:
-            ShakeCast> import_facility_xml /full/path/to/facility_file.xml
-            ShakeCast> import_group_xml /full/path/to/group_file.xml
-            ShakeCast> import_user_xml /full/path/to/user_file.xml
+        
+    3. Start USGS monitor (geo_json) and shakemap monitor (check_new_shakemaps) from within the CLI:
+    ::
+        ShakeCast> start
+            
+    4. Upload user data from within CLI: (API under construction and *not ready for use*)
+    ::
+        ShakeCast> import_facility_xml /full/path/to/facility_file.xml
+        ShakeCast> import_group_xml /full/path/to/group_file.xml
+        ShakeCast> import_user_xml /full/path/to/user_file.xml
+        
+        
 """
