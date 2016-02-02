395eedeeb1ef4c993691d94d0235bbc924a522de
Daniel A Slosky <dslosky@usgs.gov>
2016-02-02 11:43:19 -0700

    previous commit ruined layout of code indentation

--- sc/app/__init__.py
+++ sc/app/__init__.py
@@ -3,22 +3,27 @@ This app package contains all the code written for pyCast
 (ShakeCast V4).
 
 Required packages:
-    1. SQLAlchemy::
+    1. SQLAlchemy
+    ::
         pip install sqlalchemy
 
 Start the application after pulling from source:
-    1. Start the server::
+    1. Start the server
+    ::
         $ cd /some/dir/shakecast_v4/sc/app/
         $ python server.py
         
-    2. Start the CLI::
+    2. Start the CLI
+    ::
         $ cd /some/dir/shakecast_v4/sc/        
         $ python ui.py
         
-    3. Start USGS monitor (geo_json) and shakemap monitor (check_new_shakemaps) from within the CLI:::
+    3. Start USGS monitor (geo_json) and shakemap monitor (check_new_shakemaps) from within the CLI:
+    ::
         ShakeCast> start
             
-    4. Upload user data from within CLI: (API under construction and *not ready for use*)::
+    4. Upload user data from within CLI: (API under construction and *not ready for use*)
+    ::
         ShakeCast> import_facility_xml /full/path/to/facility_file.xml
         ShakeCast> import_group_xml /full/path/to/group_file.xml
         ShakeCast> import_user_xml /full/path/to/user_file.xml
