693deb4af2d801149d929127beaff351ae26fcf7
Daniel A Slosky <dslosky@usgs.gov>
2016-03-01 13:19:24 -0700

    Got timestamps working in server template

--- sc/web_server.py
+++ sc/web_server.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, url_for
+import time
+import datetime
 from app.dbi.db_alchemy import *
 from app.server import Server
 from app.functions_util import *
@@ -10,9 +12,11 @@ app = Flask(__name__,
 def index():
     session = Session()
     
-    eqs = session.query(ShakeMap).all()
-    #eq_lst = [str(eq) for eq in eqs]
-    return render_template('index.html', eqs=eqs)
+    eqs = session.query(Event).order_by(Event.time.desc())
+    datetimes = []
+    for eq in eqs:
+        datetimes += [datetime.datetime.fromtimestamp(eq.time/1000.0).strftime('%Y-%m-%d %H:%M:%S')]
+    return render_template('index.html', eqs_times=zip(eqs,datetimes))
 
 @app.route('/dbtest')
 def db_test():
