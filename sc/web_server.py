c601799f4ed6dd9441e1215183dfa01e4883668a
Daniel A Slosky <dslosky@usgs.gov>
2016-03-22 12:05:36 -0600

    Web server uses application time through clock and has an optional flag to trigger debugging

--- sc/web_server.py
+++ sc/web_server.py
@@ -11,6 +11,7 @@ import datetime
 from app.dbi.db_alchemy import *
 from app.server import Server
 from app.functions_util import *
+from app.objects import Clock
 app = Flask(__name__,
             template_folder=sc_dir()+'view'+get_delim()+'html',
             static_folder=sc_dir()+'view'+get_delim()+'static')
@@ -18,11 +19,12 @@ app = Flask(__name__,
 @app.route('/')
 def index():
     session = Session()
-    
+    clock = Clock()
     eqs = session.query(Event).order_by(Event.time.desc())
+    
     datetimes = []
     for eq in eqs:
-        datetimes += [datetime.datetime.fromtimestamp(eq.time/1000.0).strftime('%Y-%m-%d %H:%M:%S')]
+        datetimes += [clock.from_time(eq.time).strftime('%Y-%m-%d %H:%M:%S')]
     return render_template('index.html', eqs_times=zip(eqs,datetimes))
 
 @app.route('/dbtest')
@@ -37,4 +39,9 @@ def db_test():
 
     return return_str
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+    if len(sys.argv) > 1:
+        if sys.argv[1] == '-d':
+            # run in debug mode
+            app.run(host='0.0.0.0', port=80, debug=True)
+    else:
+        app.run(host='0.0.0.0', port=80)
