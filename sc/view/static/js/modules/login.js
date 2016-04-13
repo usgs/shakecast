b3498e1e81ff23a6b4223c66e477e6e94612fadd
Daniel A Slosky <dslosky@usgs.gov>
2016-04-12 22:57:28 -0600

    Add login forms

---
+++ sc/view/static/js/modules/login.js
@@ -0,0 +1,19 @@
+var app = angular.module('login', ['ngRoute', 'ngAnimate']);
+
+app.config(['$interpolateProvider', function($interpolateProvider) {
+  $interpolateProvider.startSymbol('{[');
+  $interpolateProvider.endSymbol(']}');
+}]);
+
+app.config(function($routeProvider){
+  $routeProvider
+    .when('/login',{
+        templateUrl:'/login',
+        controller: 'loginController'
+    })
+    .when('/register',{
+        templateUrl:'/register',
+        controller: 'loginController'
+    })
+
+});
