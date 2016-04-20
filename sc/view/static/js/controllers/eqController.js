8decf2687296027c63734acee3545fed6157ac0b
Daniel A Slosky <dslosky@usgs.gov>
2016-04-19 23:55:42 -0600

    EQ click repositions map and shows new data

--- sc/view/static/js/controllers/eqController.js
+++ sc/view/static/js/controllers/eqController.js
@@ -1,4 +1,4 @@
-app.controller('eqController', function($scope, $http) {
+app.controller('eqController', function($scope, $http, $timeout) {
     // create a message to display in our view
     $http.get('/get/eqdata')
         .then(
@@ -6,12 +6,7 @@ app.controller('eqController', function($scope, $http) {
                 $scope.eq_data = response.data
                 $scope.cur_eq = $scope.eq_data[0]
                 
-                angular.extend($scope, {
-                    center: {
-                        lat: $scope.cur_eq.lat,
-                        lng: $scope.cur_eq.lon,
-                        zoom: 4}
-                });
+                $scope.loadEQ(0)
             }, 
             function(response){
               $scope.eq_data = 'None'
@@ -28,19 +23,48 @@ app.controller('eqController', function($scope, $http) {
         }
     });
     
-    $scope.loadEQ = function(index) {
-        $scope.cur_eq = $scope.eq_data[index]
-    
-        angular.extend($scope, {
-                    center: {
-                        lat: $scope.cur_eq.lat,
-                        lng: $scope.cur_eq.lon,
-                        zoom: 4},
+    // set default map values that will be overwritten when eq data
+    // loads
+    angular.extend($scope, {
                     defaults: {
                         scrollWheelZoom: false
+                    },
+                    eqCenter: {
+                        lat: 32,
+                        lng: -122,
+                        zoom: 4
+                    },
+                    markers: {
+                        eqMarker: {
+                            lat: 0,
+                            lng: 0,
+                            message: '',
+                            focus: false,
+                            draggable: false
+                        }
                     }
                 });
+    
+    $scope.loadEQ = function(index) {
+        // apply new eq data to the map
+        $timeout(function () {
+            $scope.cur_eq = $scope.eq_data[index]
+            $scope.eqCenter = {
+                            lat: $scope.cur_eq.lat,
+                            lng: $scope.cur_eq.lon,
+                            zoom: 4
+                        };
+            $scope.markers = {
+                            eqMarker: {
+                                lat: $scope.cur_eq.lat,
+                                lng: $scope.cur_eq.lon,
+                                message: $scope.cur_eq.place,
+                                focus: false,
+                                draggable: false
+                            }
+                        };
+        }, 100);
     }
     
-    $scope.locked = []
+    
 });
