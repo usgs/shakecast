9e24d964d2b056aa40dfec13b8edc29d843cec59
Slosky <dslosky@usgs.gov>
2016-06-30 12:19:35 -0600

    Runs top-locking function when page loads, closes #59

--- sc/view/static/js/directives/topLockDirective.js
+++ sc/view/static/js/directives/topLockDirective.js
@@ -1,18 +1,18 @@
-app.directive('topLock', function ($window) {
-    var $win = angular.element($window); // wrap window object as jQuery object
-    //locked_height = 0
+app.directive('topLock', function ($window, $timeout) {
     
+    var $win = angular.element($window); // wrap window object as jQuery object
     return {
         restrict: 'A',
         link: function (scope, element, attrs) {
-            var topClass = attrs.topLock, // get CSS class from directive's attribute value
-                name = attrs.divName,
-                offsetTop = element.prop('offsetTop'), // get element's offset top relative to document
-                padTop = parseInt(attrs.paddingTop, 10),
-                parent = element.parent(),
-                offsetTop;
-                
-            $win.on('scroll', function (e) {
+            
+            var top_lock = function() {
+                var topClass = attrs.topLock, // get CSS class from directive's attribute value
+                    name = attrs.divName,
+                    offsetTop = element.prop('offsetTop'), // get element's offset top relative to document
+                    padTop = parseInt(attrs.paddingTop, 10),
+                    parent = element.parent(),
+                    offsetTop;
+                    
                 offsetTop = parent.prop('offsetTop') - padTop;
                 if (($window.pageYOffset >= offsetTop) && ($window.pageYOffset != 0)) {
                     element.addClass(topClass);
@@ -32,7 +32,10 @@ app.directive('topLock', function ($window) {
                         //element.removeClass('locked-navbar', {duration:200, children: true})
                     }
                 }
-            });
+            }
+            
+            $win.on('scroll', function(e) {top_lock()});
+            top_lock()
         } 
     };
 });
