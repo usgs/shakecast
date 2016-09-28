ac9b01f76509e61e81db051110ad6c34a7dd32d0
dslosky-usgs <dslosky@usgs.gov>
2016-09-28 11:39:58 -0600

    Tracking div height
    Allows us to remove the height from the stt service on destroy

--- sc/view/app/shared/directives/stick-to-top.directive.ts
+++ sc/view/app/shared/directives/stick-to-top.directive.ts
@@ -1,8 +1,6 @@
 import { Directive, 
          ElementRef, 
-         Input, 
-         Renderer, 
-         onInit } from '@angular/core';
+         OnDestroy } from '@angular/core';
 
 import { Observable } from 'rxjs/Observable';
 import { StickToTopService } from './stick-to-top.service'
@@ -14,25 +12,30 @@ import { StickToTopService } from './stick-to-top.service'
             '(window:scroll)': 'checkLock($event)'}
 })
 
-export class StickToTopDirective implements onInit {
-    private scrolled: number = document.querySelector('body').scrollTop
-    public stuck: boolean = false
-    public stuckTop: number = 0
-    public top: number = 0
-
+export class StickToTopDirective implements OnDestroy {
+    private scrolled: number = document.querySelector('body').scrollTop;
+    public stuck: boolean = false;
+    public stuckTop: number = 0;
+    public top: number = 0;
+    private height: number = 0;
+    private init: boolean = true
     constructor(private el:ElementRef,
                 private sttService: StickToTopService) {
-        this.top = el.nativeElement.offsetTop
+        this.top = el.nativeElement.offsetTop;
     }
 
     checkLock(event: any) {
+        if (this.init) {
+            this.init = false
+            this.height = this.el.nativeElement.parentElement.offsetHeight
+        }
         this.scrolled = document.querySelector('body').scrollTop
         if (this.stuck === true) {
-            if (this.el.nativeElement.parentElement.offsetTop + this.el.nativeElement.clientHeight >= 
+            if (this.el.nativeElement.parentElement.offsetTop + this.height >= 
                     this.scrolled + this.sttService.stackHeight) {
                 console.log('Unstick it')
                 this.stuckTop = this.top
-                this.sttService.stackHeight -= this.el.nativeElement.clientHeight
+                this.sttService.stackHeight -= this.height
                 this.stuck = false
             }
         } else if (this.scrolled >= 
@@ -40,9 +43,13 @@ export class StickToTopDirective implements onInit {
             if (this.stuck !== true) {
                 console.log('Stick it')
                 this.stuckTop = this.sttService.stackHeight
-                this.sttService.stackHeight += this.el.nativeElement.parentElement.offsetHeight
+                this.sttService.stackHeight += this.height
                 this.stuck = true
             }
         }
+    }
+
+    ngOnDestroy() {
+        this.sttService.stackHeight -= this.height
     }
 }
