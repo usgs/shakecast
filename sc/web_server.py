03725fdf6ec51cee3a10bdfb650d8c31f8df1a09
dslosky-usgs <dslosky@usgs.gov>
2016-12-23 04:30:32 -0700

    Groups will filter by user

--- sc/web_server.py
+++ sc/web_server.py
@@ -197,10 +197,13 @@ def delete_facilities():
 @login_required
 def get_groups():
     session = Session()
-    groups = (session.query(Group)
-                .filter(Group.shakecast_id > request.args.get('last_id', 0))
-                .limit(50)
-                .all())
+    filter_ = json.loads(request.args.get('filter', '{}'))
+    query = session.query(Group)
+    if filter_:
+        if filter_.get('user', None):
+            query = query.filter(Group.users.any(User.username == filter_['user']))
+
+    groups = query.all()
     
     group_dicts = []
     for group in groups:
