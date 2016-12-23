a93298e9b3c8656734ffb6204557ae388b841699
dslosky-usgs <dslosky@usgs.gov>
2016-12-23 03:29:26 -0700

    User service and list working
    Updated list css to be generalized... group, facility, eq pages can be updated with these new rules

--- sc/view/app/shakecast-admin/pages/users/user-list.component.ts
+++ sc/view/app/shakecast-admin/pages/users/user-list.component.ts
@@ -1,9 +1,76 @@
-import { Component } from '@angular/core';
+import { Component,
+         OnInit, 
+         OnDestroy,
+         trigger,
+         state,
+         style,
+         transition,
+         animate } from '@angular/core';
 
+import { UsersService, User } from './users.service'
 @Component({
-  selector: 'user-list',
-  templateUrl: 'app/shakecast-admin/pages/users/user-list.component.html',
-  styleUrls: ['app/shakecast-admin/pages/users/user-list.component.css'], 
+    selector: 'user-list',
+    templateUrl: 'app/shakecast-admin/pages/users/user-list.component.html',
+    styleUrls: ['app/shakecast-admin/pages/users/user-list.component.css'],
+    animations: [
+      trigger('selected', [
+        state('true', style({transform: 'translateY(-10px)'})),
+        state('false', style({transform: 'translateY(0px)'})),
+          transition('true => false', animate('100ms ease-out')),
+          transition('false => true', animate('100ms ease-in'))
+      ]),
+      trigger('headerSelected', [
+        state('true', style({'background-color': '#7af'})),
+        state('false', style({'background-color': '#aaaaaa'})),
+          transition('true => false', animate('100ms ease-out')),
+          transition('false => true', animate('100ms ease-in'))
+      ])
+
+    ]
 })
-export class UserListComponent {
+export class UserListComponent implements OnInit, OnDestroy {
+    public loadingData: boolean = false;
+    public dataList: any = [];
+    public filter: filter = {};
+    public selected: User;
+    private subscriptions: any[] = [];
+
+    constructor(private userService: UsersService) {}
+
+    ngOnInit() {
+        this.subscriptions.push(this.userService.userData.subscribe(data => {
+            this.dataList = data;
+            for (var user in this.dataList) {
+                this.dataList[user].selected = false;
+                this.selected = this.dataList[0];
+                this.selected.selected = true;
+
+                //this.userService.plotGroup(this.groupData[0])
+            }
+        }));
+
+        this.subscriptions.push(this.userService.loadingData.subscribe(loading => {
+            this.loadingData = loading;
+        }));
+
+        this.userService.getData();
+
+    }
+
+    clickData(data: any) {
+        this.selected.selected = false;
+        data.selected = true;
+        this.selected = data;
+    }
+
+    ngOnDestroy() {
+        this.endSubscriptions()
+    }
+
+    endSubscriptions() {
+        for (var sub in this.subscriptions) {
+            this.subscriptions[sub].unsubscribe();
+        }
+    }
+
 }
