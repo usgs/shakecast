f16dd1c28bc3b37e3a5db5e02f6f650fd2fb4068
dslosky-usgs <dslosky@usgs.gov>
2017-02-02 06:47:37 -0700

    Improved stick-to-top div behavior

--- sc/view/app/shared/directives/stick-to-top.directive.ts
+++ sc/view/app/shared/directives/stick-to-top.directive.ts
@@ -19,31 +19,41 @@ export class StickToTopDirective implements OnInit, OnDestroy {
     public top: number = 0;
     private height: number = 0;
     private init: boolean = true
+    private didScroll: boolean = false;
+
     constructor(private el:ElementRef,
                 private sttService: StickToTopService) {
         this.top = el.nativeElement.offsetTop;
     }
 
     ngOnInit() {
-        this.checkLock({})
+        this.checkLock()
+        Observable.interval(50)
+                .subscribe(x => {
+            if(this.didScroll) {
+                this.didScroll = false;
+                this.checkLock();
+            }
+        });
     }
 
-    checkLock(event: any) {
+    checkLock(event: Event = null) {
         if (this.init) {
             this.init = false
             this.height = this.el.nativeElement.parentElement.offsetHeight
         }
         this.scrolled = document.querySelector('body').scrollTop
         if (this.stuck === true) {
-            if (this.el.nativeElement.parentElement.offsetTop + this.height >= 
+            if (this.el.nativeElement.parentElement.getBoundingClientRect().top + this.scrolled + this.height >= 
                     this.scrolled + this.sttService.stackHeight) {
                 //console.log('Unstick it')
                 this.stuckTop = this.top
                 this.sttService.stackHeight -= this.height
                 this.stuck = false
             }
-        } else if (this.scrolled >= 
-                    (this.el.nativeElement.parentElement.offsetTop - this.sttService.stackHeight)) {
+        } else if ((this.scrolled >= 
+                    (this.el.nativeElement.parentElement.getBoundingClientRect().top + this.scrolled - this.sttService.stackHeight)) 
+                    || this.el.nativeElement.parentElement.getBoundingClientRect().top < 0) {
             if (this.stuck !== true) {
                 //console.log('Stick it')
                 this.stuckTop = this.sttService.stackHeight
