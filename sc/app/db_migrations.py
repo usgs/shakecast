b7e5d8ae95bb7c058d15d48cc27532d0dd123e6a
dslosky-usgs <dslosky@usgs.gov>
2017-06-14 16:05:08 -0700

    Found and fixed migration error
    'group' is a reserved name for sqlite

--- sc/app/db_migrations.py
+++ sc/app/db_migrations.py
@@ -33,7 +33,7 @@ def add_column(engine, table_name, column):
     '''
     column_name = column.compile(dialect=engine.dialect)
     column_type = column.type.compile(engine.dialect)
-    engine.execute('ALTER TABLE %s ADD COLUMN %s %s' % (table_name, column_name, column_type))
+    engine.execute('ALTER TABLE "%s" ADD COLUMN %s %s' % (table_name, column_name, column_type))
 
 # List of database migrations for export
 migrations = [migrate_1to2]
