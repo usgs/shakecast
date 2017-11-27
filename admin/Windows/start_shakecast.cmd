b8dc573b5208965d15b31f149e0893b962d9f6b4
Daniel Slosky <dslosky@usgs.gov>
2017-11-27 08:50:32 -0800

    set services reboot as 'auto', closes #190

--- admin/Windows/start_shakecast.cmd
+++ admin/Windows/start_shakecast.cmd
@@ -1,5 +1,5 @@
-python "%userprofile%\Shakecast\shakecast\admin\Windows\server_service.py" install
-python "%userprofile%\Shakecast\shakecast\admin\Windows\web_server_service.py" install
+python "%userprofile%\Shakecast\shakecast\admin\Windows\server_service.py" --startup=auto install
+python "%userprofile%\Shakecast\shakecast\admin\Windows\web_server_service.py" --startup=auto install
 
 python "%userprofile%\Shakecast\shakecast\admin\Windows\server_service.py" start
 python "%userprofile%\Shakecast\shakecast\admin\Windows\web_server_service.py" start
