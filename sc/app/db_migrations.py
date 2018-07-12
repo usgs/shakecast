3b7912c4e9ee68b713a795a2e40fa036ab5bd207
dslosky-usgs <dslosky@usgs.gov>
2018-07-12 12:50:40 -0600

    Change ADD COLUMN syntax for mysql

--- sc/app/db_migrations.py
+++ sc/app/db_migrations.py
@@ -55,7 +55,11 @@ def add_column(engine, table_name, column):
     '''
     column_name = column.compile(dialect=engine.dialect)
     column_type = column.type.compile(engine.dialect)
-    engine.execute('ALTER TABLE "%s" ADD COLUMN %s %s' % (table_name, column_name, column_type))
+
+    if 'sqlite' in str(engine):
+        engine.execute('ALTER TABLE "%s" ADD COLUMN %s %s' % (table_name, column_name, column_type))
+    elif 'mysql' in str(engine):
+        engine.execute('ALTER TABLE `%s` ADD COLUMN %s %s' % (table_name, column_name, column_type))
 
 # List of database migrations for export
 migrations = [migrate_1to2, migrate_2to3]
