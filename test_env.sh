7ef5f7a374ce931cd8dd8d929f24f09b441b7c31
dslosky-usgs <dslosky@usgs.gov>
2020-12-16 14:05:09 -0500

    Add app startup to test_env.sh

--- test_env.sh
+++ test_env.sh
@@ -4,6 +4,8 @@ export SC_SMTP_FROM='shakecast.ci@github.com'
 export SC_SMTP_PORT=1025 
 export SC_SMTP_SERVER='localhost'
 
-python -m shakecast.tests.smtpserver &
 python get-pip.py --trusted-host pypi.org --trusted-host files.pythonhosted.org
 pip install codecov --trusted-host pypi.org --trusted-host files.pythonhosted.org
+
+python -m shakecast.tests.smtpserver &
+python -m shakecast.app.startup
