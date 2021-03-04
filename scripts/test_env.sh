239e4695759257ac10177209f3b7abae41f3d83a
dslosky-usgs <dslosky@usgs.gov>
2021-03-04 09:57:41 -0800

    Moved test_env.sh

--- scripts/test_env.sh
+++ scripts/test_env.sh
@@ -1,8 +1,8 @@
-export TESTING_APP_ENV='testing'
-export SC_CI=1 
-export SC_SMTP_FROM='shakecast.ci@github.com' 
-export SC_SMTP_PORT=1025 
-export SC_SMTP_SERVER='localhost'
+export SHAKECAST_SMTP_FROM='shakecast.ci@github.com'
+export SHAKECAST_SMTP_PORT=1025
+export SHAKECAST_SMTP_SERVER='localhost'
+export SHAKECAST_WEB_PORT=5000
+
 
 python get-pip.py --trusted-host pypi.org --trusted-host files.pythonhosted.org
 pip install codecov --trusted-host pypi.org --trusted-host files.pythonhosted.org
