37f589c6dce4c732c42c4a2ad120d9803cfd61d1
dslosky-usgs <dslosky@usgs.gov>
2021-03-09 09:24:14 -0800

    Add smtp_send environment variable

--- shakecast/app/env.py
+++ shakecast/app/env.py
@@ -9,7 +9,7 @@ DB_CONNECTION_TYPE = os.environ.get('SHAKECAST_DB_CONNECTION_TYPE', sc.dict['DBC
 DB_CONNECTION_STRING = os.environ.get('SHAKECAST_DB_CONNECTION_STRING', f'sqlite:///{os.path.join(get_db_dir(), "shakecast.db")}')
 
 DEBUG_LEVEL = int(os.environ.get('SHAKECAST_DEBUG_LEVEL', 0))
-LOG_DIRECTORY = os.environ.get('SHAKECAST_LOG_DIRECTORY', os.path.join(SHAKECAST_DIRECTORY, 'logs'))
+LOG_DIRECTORY = os.environ.get('SHAKECAST_LOG_DIRECTORY', os.path.join(sc_dir(), 'logs'))
 
 SERVER_PORT = int(os.environ.get('SHAKECAST_SERVER_PORT', sc.dict['port']))
 SERVER_HOST_NAME = os.environ.get('SHAKECAST_SERVER_HOST_NAME', 'localhost')
@@ -23,6 +23,7 @@ SMTP_PORT = int(os.environ.get('SHAKECAST_SMTP_PORT', sc.dict['SMTP']['port']))
 SMTP_SECURITY = os.environ.get('SHAKECAST_SMTP_SECURITY', sc.dict['SMTP']['security'])
 SMTP_SERVER = os.environ.get('SHAKECAST_SMTP_SERVER', sc.dict['SMTP']['server'])
 SMTP_USERNAME = os.environ.get('SHAKECAST_SMTP_USERNAME', sc.dict['SMTP']['username'])
+SMTP_SEND_NOTIFICATIONS = os.environ.get('SHAKECAST_SMTP_SEND_NOTIFICATIONS', 1) 
 
 USER_DIRECTORY = os.environ.get('SHAKECAST_USER_DIRECTORY', get_user_dir())
 WEB_PORT = int(os.environ.get('SHAKECAST_WEB_PORT', sc.dict['web_port']))
