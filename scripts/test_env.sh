5d7ad606c4614aae9265232c318109f29910c2d5
dslosky-usgs <dslosky@usgs.gov>
2021-04-05 12:34:42 -0700

    Switch to python3 for test_env.sh

--- scripts/test_env.sh
+++ scripts/test_env.sh
@@ -3,9 +3,7 @@ export SHAKECAST_SMTP_PORT=7654
 export SHAKECAST_SMTP_SERVER='localhost'
 export SHAKECAST_WEB_PORT=5000
 
+python3 -m pip install codecov --trusted-host pypi.org --trusted-host files.pythonhosted.org
 
-python get-pip.py --trusted-host pypi.org --trusted-host files.pythonhosted.org
-pip install codecov --trusted-host pypi.org --trusted-host files.pythonhosted.org
-
-python -m shakecast.tests.smtpserver &
-python -m shakecast.app.startup
+python3 -m shakecast.tests.smtpserver &
+python3 -m shakecast.app.startup
