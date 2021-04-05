cdbdaa808183cfdf259c76e13ce7449becb5b7fe
dslosky-usgs <dslosky@usgs.gov>
2021-04-05 09:51:00 -0700

    Grabbing higher port for smtp testing

--- scripts/test_env.sh
+++ scripts/test_env.sh
@@ -1,5 +1,5 @@
 export SHAKECAST_SMTP_FROM='shakecast.ci@github.com'
-export SHAKECAST_SMTP_PORT=1025
+export SHAKECAST_SMTP_PORT=7654
 export SHAKECAST_SMTP_SERVER='localhost'
 export SHAKECAST_WEB_PORT=5000
 
