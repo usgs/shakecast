e1d92f4af87ee980e38f12d74e30f32377322f0b
dslosky-usgs <dslosky@usgs.gov>
2021-04-16 12:40:15 -0700

    Switch mock smtp server to DebuggingServer

--- scripts/python-test-setup.sh
+++ scripts/python-test-setup.sh
@@ -2,5 +2,5 @@
 
 python3 -m pip install codecov --trusted-host pypi.org --trusted-host files.pythonhosted.org
 
-python3 -m shakecast.tests.smtpserver &
+python3 -m smtpd -n -c DebuggingServer localhost:${SHAKECAST_SMTP_PORT} &
 python3 -m shakecast.app.startup
