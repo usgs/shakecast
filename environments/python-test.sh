2c2e673d879e946aabe29eb8bbb42fa3cb1f3973
dslosky-usgs <dslosky@usgs.gov>
2021-04-16 10:15:45 -0700

    Remove smtp security for testing

--- environments/python-test.sh
+++ environments/python-test.sh
@@ -2,3 +2,4 @@ export SHAKECAST_SMTP_FROM='shakecast.ci@github.com'
 export SHAKECAST_SMTP_PORT=7654
 export SHAKECAST_SMTP_SERVER='localhost'
 export SHAKECAST_WEB_PORT=5000
+export SHAKECAST_SMTP_SECURITY=''
