97e249b03913701bf0b15facc5ddcd3e98793cc8
dslosky-usgs <dslosky@usgs.gov>
2021-07-07 14:12:08 -0600

    Add origin module test

---
+++ shakecast/tests/origin/ingest.py
@@ -0,0 +1,19 @@
+import os
+import unittest
+
+from shakecast.app.origin.ingest import *
+from shakecast.app.util import get_test_dir
+
+ORIGIN_DIR = os.path.join(get_test_dir(), 'data', 'origin')
+
+class TestOrigin(unittest.TestCase):
+    def testOriginIngest(self):
+        origin_xml_path = os.path.join(ORIGIN_DIR, 'product.xml')
+        event = transform_origin_to_event(origin_xml_path)
+
+        should_import = assess_event(event)
+
+        self.assertFalse(should_import)
+
+if __name__ == '__main__':
+    unittest.main()
