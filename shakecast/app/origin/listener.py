f1bd9a6d1d5db709864a163f05efa1031f17c65e
dslosky-usgs <dslosky@usgs.gov>
2021-07-08 09:09:00 -0700

    Fix listener print

--- shakecast/app/origin/listener.py
+++ shakecast/app/origin/listener.py
@@ -11,7 +11,7 @@ class OriginConsumer(Consumer):
   QUEUE='pdl-origin'
 
   def on_message(self, _unused_channel, basic_deliver, properties, body):
-      print(f'ORIGIN RECEIVED: # {basic_deliver.delivery_tag} from {properties.app_id}: {body}')
+      print(f'ORIGIN RECEIVED: # {basic_deliver.delivery_tag}: {body}')
 
       message = json.loads(body)
       ingest(message)
