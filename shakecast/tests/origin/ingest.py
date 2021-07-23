21c4ec486e2995460dfb004d72007acde1453fa4
dslosky-usgs <dslosky@usgs.gov>
2021-07-23 10:37:02 -0600

    Origin test update

--- shakecast/tests/origin/ingest.py
+++ shakecast/tests/origin/ingest.py
@@ -8,11 +8,10 @@ ORIGIN_DIR = os.path.join(get_test_dir(), 'data', 'origin')
 
 class TestOrigin(unittest.TestCase):
     def testOriginIngest(self):
-        origin_xml_path = os.path.join(ORIGIN_DIR, 'product.xml')
-        event = transform_origin_to_event(origin_xml_path)
+        origin = get_origin_from_directory(ORIGIN_DIR)
+        event = transform_origin_to_event(origin)
 
         should_import = assess_event(event)
-
         self.assertFalse(should_import)
 
 if __name__ == '__main__':
